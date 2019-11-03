4a9d77cb731245cef8dcab31319a3ae548a3272b
Niclas <niclas.kroon@protonmail.ch>
2019-11-03 16:38:46 +0100

    remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0-alpha01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
-        classpath("com.github.jengelman.gradle.plugins:shadow:5.0.0")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
