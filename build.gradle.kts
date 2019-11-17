50fe34b38183ee85c658e77703622b0439b1f812
Niclas <niclas.kroon@protonmail.ch>
2019-11-17 14:08:48 +0100

    bump kotlin 1.3.60

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
 }
 buildscript {
-    extra["kotlin_version"] = "1.3.60-eap-25"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0-alpha01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("com.android.tools.build:gradle:4.0.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
